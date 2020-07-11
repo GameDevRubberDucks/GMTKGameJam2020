7c54c78e069c669fce725f6602865f03938438c8
Kody Wood <kodymrwood14@gmail.com>
2020-07-11 13:37:25 -0400

    [MOD] Modified scripts
    Cleaned up scripts

--- GMTK2020/Assets/Studios Assets/Scripts/Camera_Center.cs
+++ GMTK2020/Assets/Studios Assets/Scripts/Camera_Center.cs
@@ -10,39 +10,15 @@ public class Camera_Center : MonoBehaviour
     public Temp_Room[] rooms;
     public Temp_Room[] roomsAttached;
 
-    public Vector3 cameraOffset;
-    public Vector3 baseCameraPos = new Vector3 (0.0f,0.0f,-10.0f);
+    public Vector3 cameraOffset = new Vector3(0.0f,0.0f,0.0f);
+    public Vector3 baseCameraPos = new Vector3 (2.5f,0.0f,-10.0f);
     public int baseCameraSize = 5;
 
 
     void Start()
     {
-        //Finds all the rooms in the scene
-        //---Change with the right script
-        //Then need to check if they are attached
-        rooms = FindObjectsOfType<Temp_Room>();
 
-        //loops through all the rooms in the scene finds which ones are attached and places it into another list
-        for (int x = 0; x <= rooms.Length - 1; x++)
-        {
-            if (rooms[x].isAttached)
-                roomsAttached[x] = rooms[x];
-        }
-
-        //Loops through all of the attached rooms and finds the center of mass
-        //Adds them all their local positions and averages
-        for (int i = 0; i <= roomsAttached.Length - 1; i++)
-        {
-            cameraOffset += rooms[i].GetComponent<Transform>().position;
-        }
-        cameraOffset /= rooms.Length;
-
-        //Add the base cameraoffset
-        cameraOffset += baseCameraPos;
-        
-        //Move the camera
-        this.transform.position = cameraOffset;
-
+        AdjustCamera();
     }
 
     // Update is called once per frame
@@ -50,5 +26,44 @@ public class Camera_Center : MonoBehaviour
     {
         //This will change the zoom of the camera, might be an easier way but oh well
         this.GetComponent<Camera>().orthographicSize = rooms.Length + baseCameraSize;
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            AdjustCamera();
+        }
+    }
+
+
+    //call this whenever a new room is attached
+    void AdjustCamera()
+    {
+        //Reset Camera
+        cameraOffset = new Vector3(2.5f, 0.0f, 0.0f);
+        //Finds all the rooms in the scene
+        //---Change with the right script
+        //Then need to check if they are attached
+        rooms = FindObjectsOfType<Temp_Room>();
+
+        //loops through all the rooms in the scene finds which ones are attached and places it into another list
+        //for (int x = 0; x <= rooms.Length - 1; x++)
+        //{
+        //    if (rooms[x].isAttached)
+        //        roomsAttached[x] = rooms[x];
+        //}
+
+        //Loops through all of the attached rooms and finds the center of mass
+        //Adds them all their local positions and averages
+        for (int i = 0; i <= rooms.Length - 1; i++)
+        {
+            cameraOffset += rooms[i].GetComponent<Transform>().position;
+        }
+        cameraOffset /= rooms.Length;
+
+        //Add the base cameraoffset
+        cameraOffset += baseCameraPos;
+
+        //Move the camera
+        this.transform.position = cameraOffset;
     }
 }
