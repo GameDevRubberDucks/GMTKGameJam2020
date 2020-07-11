b9bb8e19c28d70fe500cf0be3c0decd9bdb893e1
Kody Wood <kodymrwood14@gmail.com>
2020-07-11 04:28:32 -0400

    [ADD] Basic Player Movement and Collision
    Added basic player movement and collision
    Added Temp colours to use
    Added room average that changes camera position and the size based on how many rooms are attached

---
+++ GMTK2020/Assets/Studios Assets/Scripts/Camera_Center.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Center : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Change with appropriate script
+    public Temp_Room[] rooms;
+    public Temp_Room[] roomsAttached;
+
+    public Vector3 cameraOffset;
+    public Vector3 baseCameraPos = new Vector3 (0.0f,0.0f,-10.0f);
+    public int baseCameraSize = 5;
+
+
+    void Start()
+    {
+        //Finds all the rooms in the scene
+        //---Change with the right script
+        //Then need to check if they are attached
+        rooms = FindObjectsOfType<Temp_Room>();
+
+        //loops through all the rooms in the scene finds which ones are attached and places it into another list
+        for (int x = 0; x <= rooms.Length - 1; x++)
+        {
+            if (rooms[x].isAttached)
+                roomsAttached[x] = rooms[x];
+        }
+
+        //Loops through all of the attached rooms and finds the center of mass
+        //Adds them all their local positions and averages
+        for (int i = 0; i <= roomsAttached.Length - 1; i++)
+        {
+            cameraOffset += rooms[i].GetComponent<Transform>().position;
+        }
+        cameraOffset /= rooms.Length;
+
+        //Add the base cameraoffset
+        cameraOffset += baseCameraPos;
+        
+        //Move the camera
+        this.transform.position = cameraOffset;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This will change the zoom of the camera, might be an easier way but oh well
+        this.GetComponent<Camera>().orthographicSize = rooms.Length + baseCameraSize;
+    }
+}
---
+++ GMTK2020/Assets/Studios Assets/Scripts/Player_Control.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour
+{
+
+    //--- Public Variables ---//
+
+    //The higher the slow the player
+    public float movementSpeed = 50.0f;
+
+    //--- Private Variables ---//
+    public Vector2 movement = new Vector2(0.0f, 0.0f);
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) / movementSpeed;
+        
+
+       if (Input.GetAxis("Interact") > 0.0f)
+        {
+            Debug.Log(Input.GetAxis("Interact"));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.position += new Vector3(movement.x, movement.y, 0.0f);
+    }
+}
