59ba1f6fd2bc12eba9bc9f187ab9b21910748253
Dan <dmaccormick@hotmail.com>
2020-07-12 13:48:15 -0400

    Made some very quick and dirty start and end screens
    Could be a LOT nicer but that's what we've got for now

---
+++ GMTK2020/Assets/Studios Assets/Scripts/Scene_Loader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Loader : MonoBehaviour
+{
+    //--- Public Variables ---/
+    public string m_sceneNameToLoad;
+    public KeyCode m_triggerKey;
+
+
+
+    //--- Unity Methods ---//
+    void Update()
+    {
+        if (Input.GetKeyDown(m_triggerKey))
+            SceneManager.LoadScene(m_sceneNameToLoad);
+    }
+}
